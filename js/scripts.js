6aaeffce1507504e797e74ad5a9e22f127f616c6
Will <student@epicodus.com>
2016-01-15 09:36:45 -0800

    Fix javascript syntax errors.

--- js/scripts.js
+++ js/scripts.js
@@ -7,49 +7,52 @@ $(function() {
     var answer3 = $("input[name=question3]:checked", "#vacation-quiz").val();
     var answer4 = $("input[name=question4]:checked", "#vacation-quiz").val();
     var answer5 = $("input[name=question5]:checked", "#vacation-quiz").val();
-    var alaska-score;
-    var hawaii-score;
-    var vegas-score;
+    var alaska_score;
+    var hawaii_score;
+    var vegas_score;
 
     //Test Scoring Calculator
-    for (answer-value = 1; answer-value < 6; answer-value ++) {
+    for (answer-value === 1; answer-value < 6; answer-value ++) {
       if (answer[answer-value] === "vegas") {
-        vegas-score += 1;
+        vegas_score += 1;
       }else if (answer[answer-value] === "hawaii") {
-        hawaii-score += 1;
+        hawaii_score += 1;
       }else if (answer[answer-value] === "alaska") {
-        alaska-score += 1;
+        alaska_score += 1;
       }
     }
 
     //Determine Test Result
-    if (alaska-score > hawaii-score && alaska-score > vegas-score) {
+    if (alaska_score > hawaii_score && alaska_score > vegas_score) {
       $("#alaska-result").show();
 
-    }if else (hawaii-score > alaska-score && hawaii-score > vegas-score) {
+    }else if (hawaii_score > alaska_score && hawaii_score > vegas_score) {
       $("#hawaii-result").show();
 
-    }if else (vegas-score > alaska-score && vegas-score > hawaii-score) {
+    }else if (vegas_score > alaska_score && vegas_score > hawaii_score) {
       $("#vegasresult").show();
 
     //Tie-Breakers
-    }if else (vegas-score = hawaii-score && vegas-score > alaska-score && hawaii-score > alaska-score) {
+  }else if (vegas_score === hawaii_score && vegas_score > alaska_score && hawaii_score > alaska_score) {
       $("#tie-result").show();
       $("#vegas-result").show();
       $("#hawaii-result").show();
 
-    }if else (vegas-score = alaska-score && vegas-score > hawaii-score && alaska-score > hawaii-score) {
+    }else if (vegas_score === alaska_score && vegas_score > hawaii_score && alaska_score > hawaii_score) {
       $("#tie-result").show();
       $("#vegas-result").show();
       $("#alaska-result").show();
 
-    }if else (alaska-score = hawaii-score && alaska-score > vegas-score && hawaii-score > vegas-score) {
+    }else if (alaska_score === hawaii_score && alaska_score > vegas_score && hawaii_score > vegas_score) {
       $("#tie-result").show();
       $("#alaska-result").show();
       $("#hawaii-result").show();
     }else {
       $("error-result").show();
     }
+
+    //Toggle quiz/results view
+    $("vacation-quiz").hide();
 
     event.preventDefault();
   });
